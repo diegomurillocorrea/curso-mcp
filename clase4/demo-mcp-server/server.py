59e2d541e76739c53c59ed0b459f93e379e62db4
diegomurillojsx <diegomurillocorrea@gmail.com>
2025-07-14 13:46:41 -0600

    Clase4 Done

---
+++ clase4/demo-mcp-server/server.py
@@ -0,0 +1,18 @@
+# server.py
+from mcp.server.fastmcp import FastMCP
+
+# Create an MCP server
+mcp = FastMCP("Demo")
+
+# Add an addition tool
+@mcp.tool()
+def sumar(a: int, b: int) -> int:
+    """Sumar dos numeros"""
+    return a + b
+
+@mcp.resource("greeting://{name}")
+def get_greeting(name: str) -> str:
+    """Get a personalized greeting"""
+    return f"Hello, {name}!"
+
+#@mcp.prompt()
